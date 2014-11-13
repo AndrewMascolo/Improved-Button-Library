9aefe46bc82a24bc7b33b5b03824fa3671cfa5f2
Andrew Mascolo <adeathsk8r@comcast.net>
2014-11-12 22:09:53 -0500

    Added SetDebounceTime function

--- Button.h
+++ Button.h
@@ -21,7 +21,7 @@ class Button
 {
   private:
     bool            State, lastState;
-    unsigned long   onTime, holdTime, heldTime;
+    unsigned long   onTime, holdTime, heldTime, DBTime, DBInterval;
     byte            _P;
     byte            ButtonState;
     void            (*F_on)();
@@ -31,13 +31,18 @@ class Button
   public:
     Button(byte Precision = LOW) : _P(Precision) { } // precision default = LOW or Milliseconds
 
-    void SetStateAndTime(byte S = HIGH, double T = 500)
+    void SetStateAndTime(byte S = HIGH, unsigned long T = 500)
     {
       State = S; // Set the preferred state of the button. (Is the button HIGH or LOW when pressed?)
       lastState = !State; //lastState should be inverted from State
       holdTime = _P ? (T * 1000) : T; // Set the hold time in seconds
     }
 
+	void SetDebounceTime(unsigned long T = 50)
+	{
+	  DBInterval = _P ? (T * 1000) : T;
+	}
+	
     void onPressed(void (*ON)() )
     {
       F_on = ON;
@@ -59,11 +64,12 @@ class Button
       // Button has not been pressed yet
       ButtonState = WAITING;
       _button = digitalRead(_buttonPin);
-	  
+	    
       // First check to see if the button is pressed ie. State -> (LOW or HIGH)
       // and if it is different from it's lastState
       if (_button == State && lastState == !State)
       {
+	    DBTime = _P? micros() : millis();
         // record time
         onTime = _P ? micros() : millis();
 
@@ -78,29 +84,30 @@ class Button
         // Button was pressed
         ButtonState = PRESSED;
       }
-
-      // button is still held down
-      if (_button == State && lastState == State)
-      {
-        // check to see if button is held down for X seconds
-        if ( (heldTime = ((_P ? micros() : millis()) - onTime)) >= holdTime) // 1 second is 1,000 milliseconds or 1,000,000 microseconds
+      if((_P ? micros() : millis()) - DBTime  >= DBInterval)
+	  {
+        // button is still held down
+        if (_button == State && lastState == State)
         {
-          if (*F_hold) // if something was assigned to that function, do it
-            F_hold(); // if button is held down for X seconds, execute the HOLD function
+          // check to see if button is held down for X seconds
+          if ( (heldTime = ((_P ? micros() : millis()) - onTime)) >= holdTime) // 1 second is 1,000 milliseconds or 1,000,000 microseconds
+          {
+            if (*F_hold) // if something was assigned to that function, do it
+              F_hold(); // if button is held down for X seconds, execute the HOLD function
 
-          ButtonState = HELD;
+            ButtonState = HELD;
+          }
+        }
+
+        if (_button == !State && lastState == State)//Button was released
+        {
+          if (*F_off) // if something was assigned to that function, do it
+            F_off(); // otherwise execute the OFF function
+
+          ButtonState = RELEASED;
+          lastState = !State; // Update lastState
         }
       }
-
-      if (_button == !State && lastState == State)//Button was released
-      {
-        if (*F_off) // if something was assigned to that function, do it
-          F_off(); // otherwise execute the OFF function
-
-        ButtonState = RELEASED;
-        lastState = !State; // Update lastState
-      }
-      
       return ButtonState;
     }
 	
